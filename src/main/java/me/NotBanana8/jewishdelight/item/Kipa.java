4d3669c54979a9a34d8afc056f6b8f8bcaf0bd36
NotBanana8 <gal.loya48@gmail.com>
2024-11-23 16:34:02 +0200

    New items and recipes: -Added Gefilte Fish -Added the Gefilte Fish recipe -Added Sliced Carrot -Added the Sliced Carrot recipe

--- src/main/java/me/NotBanana8/jewishdelight/item/Kipa.java
+++
@@ -1,9 +0,0 @@
-package me.NotBanana8.jewishdelight.item;
-
-import net.minecraft.world.item.Item;
-
-public class Kipa extends Item {
-    public Kipa(Properties pProperties) {
-        super(pProperties);
-    }
-}
