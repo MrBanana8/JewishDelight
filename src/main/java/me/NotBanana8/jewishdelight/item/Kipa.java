5e1921223355046d96e60b26e0548716338827be
NotBanana8 <gal.loya48@gmail.com>
2024-11-23 16:32:18 +0200

    New items and recipes: -Added Gefilte Fish -Added the Gefilte Fish recipe -Added Sliced Carrot -Added the Sliced Carrot recipe

---
+++ src/main/java/me/NotBanana8/jewishdelight/item/Kipa.java
@@ -0,0 +1,9 @@
+package me.NotBanana8.jewishdelight.item;
+
+import net.minecraft.world.item.Item;
+
+public class Kipa extends Item {
+    public Kipa(Properties pProperties) {
+        super(pProperties);
+    }
+}
--- src/main/java/me/NotBanana8/jewishdelight/item/ModItems.java
+++ src/main/java/me/NotBanana8/jewishdelight/item/ModItems.java
@@ -3,6 +3,7 @@ package me.NotBanana8.jewishdelight.item;
 import me.NotBanana8.jewishdelight.JewishDelight;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.food.Foods;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +17,9 @@ public class ModItems {
             .food(new FoodProperties.Builder().nutrition(3).build())));
     public static final RegistryObject<Item> SLICED_CARROT = ITEMS.register("sliced_carrot",()->new Item(new Item.Properties()
             .food(new FoodProperties.Builder().nutrition(1).build())));
+
+
+    public static final RegistryObject<Item> KIPA = ITEMS.register("kipa",()-> new Kipa());
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
